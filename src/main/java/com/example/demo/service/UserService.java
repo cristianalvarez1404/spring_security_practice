e99bc62d65acc5c3be63db457604455a361b9bf5
cristian <cristianalvarez1496@gmail.com>
2026-03-11 18:37:23 -0500

    brcrypt

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,23 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Users;
+import com.example.demo.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    public Users register(Users user){
+        user.setPassword(encoder.encode(user.getPassword()));
+        return userRepo.save(user);
+    }
+}
